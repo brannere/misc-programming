b991f4414e9d5a4352ae2fe5ede51ac643623cb3
Erick Branner <eobrann@gmail.com>
2021-12-08 20:43:39 -0800

    day2 part2

--- advent-of-code/2021/day2/main.cpp
+++ advent-of-code/2021/day2/main.cpp
@@ -4,25 +4,33 @@ using std::cin;
 using std::cout;
 using std::string;
 
-int main(){
+int main(int argc, char* argv[]){
+
   string word;
-  int num;
-  int h_pos = 0;
-  int depth = 0;
+  bool isTaskPart1 = true;
+  long long int num = 0;
+  long long int h_pos = 0;
+  long long int depth = 0;
+  long long int aim = 0;
+
+  if(argc > 1 && argv[1][0] == '2') isTaskPart1 = false;
 
   cin >> word;
   for(;cin >> num;){
     switch(word[0]){
       case 'f':
         h_pos += num;
+        depth += (aim*num);
         break;
       
       case 'd':
-        depth += num;
+        if(isTaskPart1) depth += num;
+        aim += num;
         break;
       
       case 'u':
-        depth -= num;
+        if(isTaskPart1) depth -= num;
+        aim -= num;
         break;
 
       default:
@@ -33,6 +41,7 @@ int main(){
   }
   cout << "Horizontal: " << h_pos << "\n";
   cout << "Depth: " << depth << "\n";
+  cout << "Aim: " << depth << "\n";
   cout << "Product: " << depth*h_pos << "\n";
 
   return 0;
