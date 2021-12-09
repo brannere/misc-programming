c9c9c539176067ec2efb00dbdbc32de72e5634ce
Erick Branner <eobrann@gmail.com>
2021-12-08 20:01:28 -0800

    day1 part2

--- advent-of-code/2021/day1/main.cpp
+++ advent-of-code/2021/day1/main.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
 using std::cout;
 using std::cin;
-int main(){
+int main(int argc, char* argv[]){
   int num = 0;
   int prev = 0;
   int count = 0;
   
-  cin >> prev;
-  for(;cin >> num;){
-    if((num - prev) > 0) count += 1;
-    prev = num;
+  if(argv[1][0] == '1'){
+    /* Part 1 */
+    cin >> prev;
+    for(;cin >> num;){
+      if((num - prev) > 0) count += 1;
+      prev = num;
+    }
+    cout << "Part 1: increased: " << count <<" times\n";
+
+    count = 0;
   }
-  cout << "increased: " << count <<" times\n";
+  else if(argv[1][0] == '2'){
+    /* Part 2 */
+    int one, two, three, next;
+    cin >> one;
+    cin >> two;
+    cin >> three;
+
+    for(;cin >> next;){
+      if((two+three+next) - (one + two + three) > 0){
+        count += 1;
+      }
+      one = two;
+      two = three;
+      three = next;
+    }
+
+
+    cout << "Part 2: increased: " << count <<" times\n";
+  }
   return 0;
 }
